6e76c327ddb0c83aa06e32697679b8693d9b1ecc
Ahuma05 <161934421+Ahuma05@users.noreply.github.com>
2026-02-11 18:43:27 +0000

    Final finishes

--- widgets/grocery_list.dart
+++ widgets/grocery_list.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,7 +14,70 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  var _isloading = true;
+  String? error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+      'flutter-prep-52531-default-rtdb.firebaseio.com',
+      'shopping-list.json',
+    );
+
+    try {
+      final response = await http.get(url);
+
+      // 1. Handle Firebase "null" string or empty body
+      if (response.body == 'null' || response.statusCode >= 400) {
+        setState(() {
+          _isloading = false;
+        });
+        return;
+      }
+
+      if (response.statusCode >= 400) {
+        setState(() {
+          error = 'Failed to fetch data.Please try again later';
+        });
+      }
+      final Map<String, dynamic> listData = jsonDecode(response.body);
+      final List<GroceryItem> loadedItems = [];
+
+      for (final entry in listData.entries) {
+        // Find the category object based on the title stored in Firebase
+        final category = categories.entries
+            .firstWhere(
+              (catItem) => catItem.value.title == entry.value['category'],
+            )
+            .value;
+
+        loadedItems.add(
+          GroceryItem(
+            id: entry.key,
+            name: entry.value['name'],
+            quantity: entry.value['quantity'],
+            category: category,
+          ),
+        );
+      }
+
+      setState(() {
+        _groceryItems = loadedItems;
+        _isloading = false;
+      });
+    } catch (err) {
+      // This catches network issues or parsing errors
+      setState(() {
+        error = 'Failed to fetch data.Please try again later';
+      });
+    }
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(
@@ -20,28 +87,41 @@ class _GroceryListState extends State<GroceryList> {
     if (newItem == null) {
       return;
     }
-
     setState(() {
       _groceryItems.add(newItem);
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
+
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https(
+      'flutter-prep-52531-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = Scaffold(
-      body: Center(
-        child: Text(
-          'You have no items added yet',
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-      ),
+      body: Center(child: Text('You have no items added yet')),
     );
+
+    if (_isloading) {
+      content = const Center(child: CircularProgressIndicator());
+    } // showing a circular loading sign when coditon is true......
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
@@ -64,6 +144,10 @@ class _GroceryListState extends State<GroceryList> {
         ),
       );
     } // displaying this conditional when you have no items added yet
+
+    if (error != null) {
+      content = Center(child: Text(error!));
+    }
 
     return Scaffold(
       appBar: AppBar(
--- widgets/new_item.dart
+++ widgets/new_item.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/categories.dart';
+import 'package:http/http.dart' as http;
 import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
@@ -14,31 +17,56 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   final _formKey =
       GlobalKey<
         FormState
       >(); // used to save,reset & validate form fields decendants....
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!
         .validate()) // runs validations in all form sections and returns a bool.....
     {
       _formKey.currentState!
           .save(); // savses entered values if the validation returns true
+      setState(() {
+        _isSending == true;
+      });
+
+      final url = Uri.https(
+        'flutter-prep-52531-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      ); //creates a url to be used
+
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title,
+        }),
+      ); // sends data to my rtdb
+
+      print(response.body);
+      print(response.statusCode);
+
+      final Map<String, dynamic> resData = jsonDecode(response.body);
+
+      if (!context.mounted) {
+        return;
+      }
 
       Navigator.of(context).pop(
         GroceryItem(
-          id: DateTime.now().toString(),
+          id: resData['name'],
           name: _enteredName,
           quantity: _enteredQuantity,
           category: _selectedCategory,
         ),
       );
     }
-    print(_enteredName);
-    print(_enteredQuantity);
-    print(_selectedCategory);
   }
 
   @override
@@ -95,12 +123,12 @@ class _NewItemState extends State<NewItem> {
                   Expanded(
                     child: DropdownButtonFormField(
                       value:
-                          _selectedCategory, // 1. Tell the dropdown what is currently selected
+                          _selectedCategory, //  Tell the dropdown what is currently selected
                       items: [
                         for (final category in categories.entries)
                           DropdownMenuItem(
                             value: category
-                                .value, // 2. Assign the SPECIFIC category value here
+                                .value, //  Assign the SPECIFIC category value here
                             child: Row(
                               children: [
                                 Container(
@@ -117,7 +145,7 @@ class _NewItemState extends State<NewItem> {
                       onChanged: (value) {
                         setState(() {
                           _selectedCategory =
-                              value!; // 3. Update the state when a user picks a new one
+                              value!; //  Update the state when a user picks a new one
                         });
                       },
                     ),
@@ -129,12 +157,23 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: Text('Reset'),
                   ),
-                  ElevatedButton(onPressed: _saveItem, child: Text('Add Item')),
+                  ElevatedButton(
+                    onPressed: _isSending ? null : _saveItem,
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : Text('Add Item'),
+                  ),
                 ],
               ),
             ],
